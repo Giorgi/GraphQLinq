f4d6ceeba0696a03e9b88aa91ca24ecaa76c4376
Giorgi Dalakishvili <giorgi@aboutmycode.com>
2017-02-09 00:37:10 +0400

    Added classes for holding types schema information.

---
+++ GrapQLClientGenerator/RootObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GrapQLClientGenerator
+{
+    public class RootObject
+    {
+        public Data Data { get; set; }
+    }
+    
+    public class Data
+    {
+        [JsonProperty("__schema")]
+        public Schema Schema { get; set; }
+    }
+
+    public class Schema
+    {
+        public List<Type> Types { get; set; }
+    }
+
+    public class Type
+    {
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public string Description { get; set; }
+        public List<Field> Fields { get; set; }
+    }
+
+    public class Field
+    {
+        public string Name { get; set; }
+        public FieldType Type { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class FieldType
+    {
+        public string Kind { get; set; }
+        public string Name { get; set; }
+        public OfType OfType { get; set; }
+    }
+
+    public class OfType
+    {
+        public string Kind { get; set; }
+        public string Name { get; set; }
+    }
+}
