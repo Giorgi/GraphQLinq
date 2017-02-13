338c4e9b97cf9431d474dfa0d9a4b30c785ceeb7
Giorgi Dalakishvili <giorgi@aboutmycode.com>
2017-02-13 23:51:29 +0400

    Generate class from schema for some hardcoded url

--- GrapQLClientGenerator/Program.cs
+++ GrapQLClientGenerator/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace GrapQLClientGenerator
 {
@@ -10,6 +14,50 @@ namespace GrapQLClientGenerator
     {
         static void Main(string[] args)
         {
+            var query = @"{
+                       __schema {
+                           types {
+                             name
+                             interfaces {
+                               name
+                             }
+                             description
+                             kind
+                             enumValues{
+                               name
+                             }
+                             description
+                             fields {
+                               name
+                               description
+                               type {
+                                 name
+                                 kind
+                                 ofType {
+                                   name
+                                   kind
+                                   ofType {
+                                     name
+                                     kind
+                                     ofType {
+                                       name
+                                       kind
+                                     }
+                                   }
+                                 }
+                               }
+                             }
+                           }
+                        }
+                      }";
+
+            var webClient = new WebClient();
+            webClient.Headers.Add("Content-Type", "application/graphql");
+            var downloadString = webClient.UploadString("https://api.digitransit.fi/routing/v1/routers/finland/index/graphql", query);
+            var rootObject = JsonConvert.DeserializeObject<RootObject>(downloadString);
+
+            var graphQLClassesGenerator = new GraphQLClassesGenerator();
+            graphQLClassesGenerator.GenerateClasses(rootObject);
         }
     }
 }
