f5daed13dbcb489b1d368f1870ec62496c76544c
Giorgi Dalakishvili <giorgi@aboutmycode.com>
2017-02-16 00:48:14 +0400

    Added demo for using query

--- GraphQLinq.Demo/Program.cs
+++ GraphQLinq.Demo/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace GraphQLinq.Demo
+﻿using System.Linq;
+
+namespace GraphQLinq.Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var graphQuery = new GraphContext("https://www.superchargers.io/graphql").Locations();
+
+            //var q = graphQuery.Select(location => new { c = location.city, t = location.emails.Select(email => email.email) });
+            var t = graphQuery.Select(l => l.city);
+
+            //var locations = q.ToList();
+            var list = t.ToList();
         }
     }
 }
