027e16e455c3c34c87580e2737c37f30e386736e
Giorgi <giorgi@giorgi.dev>
2021-03-24 02:01:06 +0400

    Add Include tests

---
+++ GraphQLinq.Tests/MethodIncludeQueryGenerationTests.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using HSL;
+using NUnit.Framework;
+
+namespace GraphQLinq.Tests
+{
+    [TestFixture(Category = "Query generation tests")]
+    public class MethodIncludeQueryGenerationTests
+    {
+        HslGraphContext hslGraphContext = new HslGraphContext("");
+
+
+        [Test]
+        public void IncludingNavigationMethodQueryIncludesIncludedNavigationMethodWithNestedProperties()
+        {
+            var agencyQuery = hslGraphContext.Agency("248798")
+                .Include(agency => agency.routes.Select(route => route.trips.Select(trip => trip.stoptimesForDate("20170427"))));
+
+            Assert.That(agencyQuery.Query, Does.Contain("stoptimesForDate")
+                                           .And.Contains(nameof(Stoptime.scheduledArrival))
+                                           .And.Contains(nameof(Stoptime.realtimeArrival)));
+        }
+
+        [Test]
+        public void IncludingNavigationMethodQueryIncludesIncludedNavigationMethodParameter()
+        {
+            var agencyQuery = hslGraphContext.Agency("248798")
+                .Include(agency => agency.routes.Select(route => route.trips.Select(trip => trip.stoptimesForDate("20170427"))));
+
+            Assert.That(agencyQuery.Query, Does.Contain("$serviceDay1: String!").And.Contains("stoptimesForDate(serviceDay: $serviceDay1)"));
+        }
+
+        [Test]
+        public void IncludingNavigationMethodQueryVariablesIncludesIncludedNavigationMethodArgument()
+        {
+            var agencyQuery = hslGraphContext.Agency("248798")
+                .Include(agency => agency.routes.Select(route => route.trips.Select(trip => trip.stoptimesForDate("20170427"))));
+
+            Assert.That(agencyQuery.QueryVariables, Does.ContainKey("serviceDay1").With.ContainValue("20170427"));
+        }
+    }
+}
