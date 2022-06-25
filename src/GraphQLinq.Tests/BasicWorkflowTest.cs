f47b1e496ade9c13e428f919ae3ec589165ae84d
Stanislav Silin <silin.stanislav@outlook.com>
2022-06-25 08:17:45 +0300

    Add tests to check how errors are handled.

---
+++ src/GraphQLinq.Tests/BasicWorkflowTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GraphQLinq.Tests
+{
+    [TestFixture]
+    public class BasicWorkflowTest : TestWithTestServer
+    {
+        [Test]
+        public async Task CanExecuteQuery()
+        {
+            var user = await Context.UserTemporaryFixForNullable(42).ToItem();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("Jon", user.FirstName);
+                Assert.AreEqual("Smith", user.LastName);
+            });
+        }
+
+        [Test]
+        public void CanHandleFailedQueries()
+        {
+            Assert.CatchAsync<GraphQueryExecutionException>(async () => await Context.FailUser().ToItem());
+        }
+
+        [Test]
+        public void NullableDoesntWork()
+        {
+            Assert.CatchAsync<GraphQueryExecutionException>(async () => await Context.User(42).ToItem());
+        }
+    }
+}
---
+++ src/GraphQLinq.Tests/Core/TestWithTestServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GraphQLinq.Tests
+{
+    public class TestWithTestServer
+    {
+        public TestWithTestServer()
+        {
+            Context = new TestServer.QueryContext();
+        }
+
+        public TestServer.QueryContext Context
+        {
+            get;
+        }
+        
+        [OneTimeSetUp]
+        public async Task RunSever()
+        {
+            var server = TestServer.Program.Main(Array.Empty<string>());
+
+            var httpClient = new HttpClient();
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync("http://localhost:10000/graphql?sdl");
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        return;
+                    }
+
+                    await Task.Delay(500);
+                }
+                catch
+                {
+                }
+            }
+
+            Assert.Fail("Failed to run test graphql server.");
+        }
+
+        [OneTimeTearDown]
+        public void StopServer()
+        {
+            TestServer.Program.CancellationTokenSource.Cancel();
+        }
+
+    }
+}
